edd8abd8713d453a1beba674c4eda58d4dd50b06
Graham Cockerham <grahamco121@gmail.com>
2021-08-16 13:07:25 -0400

    add loops and iterating examples

---
+++ counter.js
@@ -0,0 +1,34 @@
+let counter = 1;
+while (counter <= 10) {
+  console.log(counter);
+  counter = counter + 1; // or counter += 1; OR counter++;
+}
+
+/* examples of succinct syntax:
+
+> let a = 3
+> a *= 4            // 3 * 4
+= 12
+
+> a /= 6            // 12 / 6
+= 2
+
+> a -= 2            // 2 - 2
+= 0  
+
+OR
+
+> let a = 1;
+> ++a;
+= 2
+
+> a
+= 2
+
+> a++
+= 2
+
+> a
+= 3
+
+*/
