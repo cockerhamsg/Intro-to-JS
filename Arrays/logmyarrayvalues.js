d843499fe46f835a12093a7202fb16f7ea7990ea
Graham Cockerham <grahamco121@gmail.com>
2021-08-20 21:49:58 -0400

    array exercises and folders

---
+++ Arrays/logmyarrayvalues.js
@@ -0,0 +1,20 @@
+// log all even values to console
+
+let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+
+for (let i = 0; i < myArray.length; i += 1) {
+  let value = myArray[i];
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+}
+
+/* OR use for forEach
+
+myArray.forEach(function(value) {
+  if (value % 2=== 0) {
+    console.log(value);
+  }
+});
+
+*/
