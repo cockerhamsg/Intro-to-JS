a57804ae89e1218432866153e9afdc882107e6b1
Graham Cockerham <grahamco121@gmail.com>
2021-08-27 21:59:42 -0400

    object exercises modifications

--- Objects/keysArray.js
+++ Objects/keysArray.js
@@ -8,8 +8,8 @@ let obj = {
 
 let objKeys = Object.keys(obj);
 let upperKeys = objKeys.map((key) => key.toUpperCase());
-console.log(upperKeys);
-console.log(obj);
+console.log(upperKeys); // => [ 'B', 'A', 'C' ]
+console.log(obj); // => { b: 2, a: 1, c: 3 }
 
 /* using forEach:
 
